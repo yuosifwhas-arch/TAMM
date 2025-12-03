a747b712c95a8328eec1be65012a50dcb9735eba
yuosifwhas-arch <yuosifwhas@gmail.com>
2025-12-03 15:48:18 +0300

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,22 +1,39 @@
+// [تعديل]: الآن نستخدم دوال import/export
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9/firebase-app.js';
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9/firebase-auth.js';
+import { getFirestore, collection, query, where, getDocs, doc, getDoc, orderBy } from 'https://www.gstatic.com/firebasejs/9/firebase-firestore.js';
+
+
 // بيانات إعداد مشروعك
 const firebaseConfig = {
-    apiKey: "AIzaSyCIjIjs-2nhLrtssISWc0pNuX2UzxhQ3ZE",
-    authDomain: "shipping-tracker-pro.firebaseapp.com",
-    projectId: "shipping-tracker-pro",
-    storageBucket: "shipping-tracker-pro.firebasestorage.app",
-    messagingSenderId: "188119609466",
-    appId: "1:188119609466:web:2f2498def75deb9905cbc3",
-    measurementId: "G-6V51EXWFMJ"
+    apiKey: "AIzaSyCIjIjs-2nhLrtssISWc0pNuX2UzxhQ3ZE",
+    authDomain: "shipping-tracker-pro.firebaseapp.com",
+    projectId: "shipping-tracker-pro",
+    storageBucket: "shipping-tracker-pro.firebasestorage.app",
+    messagingSenderId: "188119609466",
+    appId: "1:188119609466:web:2f2498def75deb9905cbc3",
+    measurementId: "G-6V51EXWFMJ"
 };
 
 // تهيئة التطبيق
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// تعريف المتغيرات العالمية (V8)
-window.auth = firebase.auth();
-window.onAuthStateChanged = firebase.auth().onAuthStateChanged.bind(firebase.auth());
-window.signInWithEmailAndPassword = firebase.auth().signInWithEmailAndPassword.bind(firebase.auth());
-window.signOut = firebase.auth().signOut.bind(firebase.auth());
+// تعريف المتغيرات المحلية (ES Modules)
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-// ** [جديد] إضافة تهيئة Firestore **
-window.db = firebase.firestore();
+// تصدير الدوال التي تحتاجها الملفات الأخرى (مثل app.js)
+export { 
+    auth, 
+    db, 
+    signInWithEmailAndPassword, 
+    onAuthStateChanged, 
+    signOut,
+    collection,
+    query,
+    where,
+    getDocs,
+    doc,
+    getDoc,
+    orderBy
+};
