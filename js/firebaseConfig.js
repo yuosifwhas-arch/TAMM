abc27e54f4fe85c6257a165dac70f89e54a9507f
yuosifwhas-arch <yuosifwhas@gmail.com>
2025-12-03 11:19:41 +0300

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,1 +1,20 @@
+// بيانات إعداد مشروعك
+const firebaseConfig = {
+    apiKey: "AIzaSyCIjIjs-2nhLrtssISWc0pNuX2UzxhQ3ZE",
+    authDomain: "shipping-tracker-pro.firebaseapp.com",
+    projectId: "shipping-tracker-pro",
+    storageBucket: "shipping-tracker-pro.firebasestorage.app",
+    messagingSenderId: "188119609466",
+    appId: "1:188119609466:web:2f2498def75deb9905cbc3",
+    measurementId: "G-6V51EXWFMJ"
+};
 
+// تهيئة التطبيق
+const app = firebase.initializeApp(firebaseConfig);
+
+// تعريف المتغيرات العالمية (لتسهيل الوصول إليها من الملفات الأخرى)
+window.auth = firebase.auth();
+window.onAuthStateChanged = firebase.auth().onAuthStateChanged.bind(firebase.auth());
+window.signInWithEmailAndPassword = firebase.auth().signInWithEmailAndPassword.bind(firebase.auth());
+window.signOut = firebase.auth().signOut.bind(firebase.auth());
+// يمكننا هنا مستقبلاً إضافة window.db = firebase.firestore();
